f00298aa883d555c74ac40605e9a32d9a3ee3b8e
codegram <omondihills02@gmail.com>
2023-04-03 11:41:10 +0300

    .

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,17 @@
 let runningTotal=0;
 let buffer="0";
 let previousOperator
-const screen= document.querySelector("#screen");
+// const screen= document.querySelector("#screen");
+
+
+// function jump(x,y){
+//     if(y==0){
+//         console.log(x);
+//         return x;
+//     }else{
+//         console.log(x - jump(x, y-1));
+//         return x - jump(x, y-1);
+//     }
+// }
+// jump(3,2)
+console.log(JSON.parse('nu'));
