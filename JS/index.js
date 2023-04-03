ef3a75ec56497b840a999223e695598e5a467808
codegram <omondihills02@gmail.com>
2023-04-03 11:54:19 +0300

    .

--- JS/index.js
+++ JS/index.js
@@ -2,9 +2,14 @@ let runningTotal=0;
 let buffer="0";
 let previousOperator;
 const screen= document.querySelector("#screen");
-function buttonClick(){}
+function buttonClick(value){
+    
+}
 function init(){
-
+    document.querySelector("#buttons")
+    .addEventListener('click', function(event){
+        buttonClick(event.target.innerText)
+    })
 }
 
 init();
