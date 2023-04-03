44f3cbc767703067559a86ebbf9a42f2682a2ea1
codegram <omondihills02@gmail.com>
2023-04-03 12:18:07 +0300

    .

--- JS/index.js
+++ JS/index.js
@@ -1,9 +1,19 @@
 let runningTotal=0;
 let buffer="0";
 let previousOperator;
-const screen= document.querySelector("#screen");
+const screen=document.querySelector("#screen");
 function buttonClick(value){
-    
+    if(typeof parseInt(value)=== 'number'){
+
+    }else{
+        
+    }
+}
+function handleSymbol(){
+
+}
+function handleNumber(){
+
 }
 function init(){
     document.querySelector("#buttons")
