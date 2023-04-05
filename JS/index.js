c88daa639d293199e2b6e148689c7f9b5f6f66e9
codegram <omondihills02@gmail.com>
2023-04-05 11:52:42 +0300

    .

--- JS/index.js
+++ JS/index.js
@@ -1,69 +1,74 @@
-let runningTotal=0;
-let buffer="0";
-let previousOperator;
-const screen=document.querySelector("#screen");
-function buttonClick(value){
-    if(isNaN(value)){
+let runningTotal = 0;
+let buffer = "0";
+let previousOperator=null;
+const screen = document.querySelector("#screen");
+function buttonClick(value) {
+    if (isNaN(value)) {
         handleSymbol(value);
-    }else{
+    } else {
         handleNumber(value)
     }
-    screen.innerText=buffer;
+    screen.innerText = buffer;
     console.log("buffer", buffer);
 }
-function handleSymbol(symbol){
-   switch(symbol){
-    case 'C':
-        buffer='0';
-        runnintTotal=0;
-        break;
-    case '+':
-    case '-':
-    case '&times;':    
-    case '&divide;':
-        handleMath(symbol)    ;
-        break;
-   }
+function handleSymbol(symbol) {
+    console.log('handleSymbol', symbol)
+    switch (symbol) {
+        case 'C':
+            buffer = '0';
+            runnintTotal = 0;
+            break;
+        case '=':
+            if(previousOperator===null){
+
+            }
+        case '+':
+        case '-':
+        case '&times':
+        case '&divide;':
+            handleMath(symbol);
+            break;
+    }
 }
-function handleMath(symbol){
-    if(buffer==="0"){
+function handleMath(symbol) {
+    if (buffer === "0") {
         return;
     }
-    const intBuffer=parseInt(buffer);
+    const intBuffer = parseInt(buffer);
 
-    if(runningTotal===0){
-        runningTotal=intBuffer;
-    }else{
+    if (runningTotal === 0) {
+        runningTotal = intBuffer;
+    } else {
         flushOperation(intBuffer)
     }
-    previousOperato=symbol;
-    buffer="0";
+    previousOperato = symbol;
+    buffer = "0";
 }
 
-function flushOperation(intBuffer){
-    if(previousOperator==='+'){
-        runningTotal+=intBuffer;
-    }else if(previousOperator==='-'){
-        runningTotal-=intBuffer;
-    }else if(previousOperator==='&times;'){
-        runningTotal*=intBuffer;
-    }else{
-        runningTotal/=intBuffer;
+function flushOperation(intBuffer) {
+    if (previousOperator === '+') {
+        runningTotal += intBuffer;
+    } else if (previousOperator === '-') {
+        runningTotal -= intBuffer;
+    } else if (previousOperator === '&times;') {
+        runningTotal *= intBuffer;
+    } else {
+        runningTotal /= intBuffer;
     }
 }
 
-function handleNumber(numberString){
-    if(buffer==='0'){
-        buffer=numberString;
-    }else{
-        buffer +=numberString;
+function handleNumber(numberString) {
+    if (buffer === '0') {
+        buffer = numberString;
+    } else {
+        buffer += numberString;
     }
 }
-function init(){
+function init() {
     document.querySelector("#buttons")
-    .addEventListener('click', function(event){
-        buttonClick(event.target.innerText)
-    })
+        .addEventListener('click', function (event) {
+            buttonClick(event.target.innerText)
+        })
 }
 
 init();
